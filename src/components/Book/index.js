89c7f30dd2d06daa656302d82fccc24ac3939cde
qinjiapeng <759848795@qq.com>
2021-04-06 13:46:17 +0800

    修复BOOK手机端显示异常

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -6,7 +6,7 @@ import styles2 from './wrapper2.module.scss';
 
 const Book = props => {
   return (
-    <div style={{ marginTop: 40, display: 'flex', justifyContent: 'space-around' }}>
+    <div style={{ marginTop: 40, display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
       <Wrapper styles={styles} />
       <Wrapper styles={styles1} mode="rotate" />
       <Wrapper styles={styles2} />
