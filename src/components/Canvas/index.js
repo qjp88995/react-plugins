53752b4075c5c7e0ed397fc7cedc75c3f992a654
qinjiapeng <759848795@qq.com>
2021-04-27 23:51:18 +0800

    实现地球绕太阳转

--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -9,29 +9,41 @@ const Canvas = props => {
   useEffect(() => {
     if (ref) {
       const stage = new Stage(ref.current);
-      const rect = new Rect({ fillStyle: '#ccc' });
-      const circle = new Circle({ fillStyle: 'red' });
-      stage.add(rect);
-      stage.add(circle);
-      stage.addEventListener('mouseOver', circle, (e, shape) => {
-        shape.options.shadowBlur = 20;
+      const space = new Rect({ x: 0, y: 0 });
+      stage.add(space);
+      const sun = new Circle({ fillStyle: '#FF4500', radius: 30, shadowColor: 'yellow', shadowBlur: 20 });
+      stage.add(sun);
+      const track = new Circle({ fillStyle: null, strokeStyle: 'rgba(255, 255, 255, 0.5)', lineWidth: 1, radius: 200 });
+      stage.add(track);
+      const earth = new Circle({ fillStyle: 'green', radius: 15, shadowColor: 'green', shadowBlur: 10 });
+      stage.add(earth);
+      let angle = 0;
+      let perAngle = 1;
+      stage.addEventListener('mouseOver', earth, (e, shape) => {
+        perAngle = 0;
       });
-      stage.addEventListener('mouseOut', circle, (e, shape) => {
-        shape.options.shadowBlur = 0;
+      stage.addEventListener('mouseOut', earth, (e, shape) => {
+        perAngle = 1;
       });
       const draw = () => {
         const canvas_rect = ref.current.getBoundingClientRect();
         const { width, height } = canvas_rect;
+        // const bgColor = stage.ctx.createLinearGradient(0, 0, width, height);
+        // bgColor.addColorStop(0, "rgba(22, 22, 5, 1)");
+        // bgColor.addColorStop(1, "rgba(45, 230, 7, 1)");
         ref.current.width = width;
         ref.current.height = height;
-        const rectWidth = 200, rectHeight = 200;
-        rect.options.x = (width - rectWidth) / 2;
-        rect.options.y = (height - rectHeight) / 2;
-        rect.options.width = rectWidth;
-        rect.options.height = rectHeight;
-        circle.options.x = width / 2;
-        circle.options.y = height / 2;
-        circle.options.radius = 60;
+        space.options.fillStyle = 'rgba(22, 22, 5, 1)';
+        space.options.width = width;
+        space.options.height = height;
+        sun.options.x = width / 2;
+        sun.options.y = height / 2;
+        track.options.x = width / 2;
+        track.options.y = height / 2;
+        earth.options.x = width / 2 + 200 * Math.cos(angle * Math.PI / 180);
+        earth.options.y = height / 2 + 200 * Math.sin(angle * Math.PI / 180);
+        angle += perAngle;
+        angle = angle > 360 ? 0 : angle;
         stage.resize(width, height);
         stage.render();
         animationId.current = requestAnimationFrame(draw);
